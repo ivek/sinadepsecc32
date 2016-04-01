1b65653cf959e759a63b79dfffe7a09578597b1b
ivek <ivekdelfin@gmail.com>
2016-04-01 16:50:39 -0600

    img emergente

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,13 @@
 function fondo(){
               document.getElementById('mision').style.display="none";
+              document.getElementById("imgs").src = "";
              
            }
 function emerge(z){
                var imgs = ["../moodlesinadep/img/Diapositiva3.png","../moodlesinadep/img/Diapositiva4.png"];
                console.log(imgs[z]);
               document.getElementById('imgs').src = imgs[z];
+              document.getElementById('imgs').alt = imgs[z];
               document.getElementById('emergente').style.display="block";
               document.getElementById('mision').style.display="block";
 }
